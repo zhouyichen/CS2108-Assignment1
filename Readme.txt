2aad0f3d5d23bc120432e421e39227c2531a9a3b
zhouyichen <zhouyichen123@gmail.com>
2016-08-29 10:08:29 +0800

    Initial code provided

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+To run this demo, please install cv2 first. (http://www.mobileway.net/2015/02/14/install-opencv-for-python-on-mac-os-x/)
+
+For the algorithm part, there is a detailed explaination, you may want to have a look.(http://www.pyimagesearch.com/2014/12/01/complete-guide-building-image-search-engine-python-opencv/) 
+
+For GUI part, you can do more ~
+
+Basically, this demo only use color histogram features which is stored in "index.csv". You can generate this file by running "index.py". To build a more powerful search engine, you can write your own "index.py" to generate better feature representation file "index.csv".
+
+Good luck~
---
+++ UI_search.py
@@ -0,0 +1,77 @@
+# import the necessary packages
+from pyimagesearch.colordescriptor import ColorDescriptor
+from pyimagesearch.searcher import Searcher
+import cv2
+from Tkinter import *
+import tkFileDialog
+from PIL import Image, ImageTk
+
+class UI_class:
+    def __init__(self, master, search_path):
+        self.search_path = search_path
+        self.master = master
+        topframe = Frame(self.master)
+        topframe.pack()
+
+        #Buttons
+        topspace = Label(topframe).grid(row=0, columnspan=2)
+        self.bbutton= Button(topframe, text=" Choose an image ", command=self.browse_query_img)
+        self.bbutton.grid(row=1, column=1)
+        self.cbutton = Button(topframe, text=" Search ", command=self.show_results_imgs)
+        self.cbutton.grid(row=1, column=2)
+        downspace = Label(topframe).grid(row=3, columnspan=4)
+
+        self.master.mainloop()
+
+
+    def browse_query_img(self):
+
+        self.query_img_frame = Frame(self.master)
+        self.query_img_frame.pack()
+        from tkFileDialog import askopenfilename
+        self.filename = tkFileDialog.askopenfile(title='Choose an Image File').name
+
+        # process query image to feature vector
+        # initialize the image descriptor
+        cd = ColorDescriptor((8, 12, 3))
+        # load the query image and describe it
+        query = cv2.imread(self.filename)
+        self.queryfeatures = cd.describe(query)
+
+        # show query image
+        image_file = Image.open(self.filename)
+        resized = image_file.resize((100, 100), Image.ANTIALIAS)
+        im = ImageTk.PhotoImage(resized)
+        image_label = Label(self.query_img_frame, image=im)
+        image_label.pack()
+
+        self.query_img_frame.mainloop()
+
+
+    def show_results_imgs(self):
+        self.result_img_frame = Frame(self.master)
+        self.result_img_frame.pack()
+
+        # perform the search
+        searcher = Searcher("index.csv")
+        results = searcher.search(self.queryfeatures)
+
+        # show result pictures
+        COLUMNS = 5
+        image_count = 0
+        for (score, resultID) in results:
+            # load the result image and display it
+            image_count += 1
+            r, c = divmod(image_count - 1, COLUMNS)
+            im = Image.open( self.search_path + "/" + resultID)
+            resized = im.resize((100, 100), Image.ANTIALIAS)
+            tkimage = ImageTk.PhotoImage(resized)
+            myvar = Label(self.result_img_frame, image=tkimage)
+            myvar.image = tkimage
+            myvar.grid(row=r, column=c)
+
+        self.result_img_frame.mainloop()
+
+
+root = Tk()
+window = UI_class(root,'dataset')
