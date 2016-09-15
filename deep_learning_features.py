062c8bd30983569a4d29d4b9e05a82a0e874f5eb
zhouyichen <zhouyichen123@gmail.com>
2016-09-15 11:45:27 +0800

    extract deep learning features

---
+++ deep_learning_features.py
@@ -0,0 +1,159 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Simple image classification with Inception.
+
+Run image classification with Inception trained on ImageNet 2012 Challenge data
+set.
+
+This program creates a graph from a saved GraphDef protocol buffer,
+and runs inference on an input JPEG image. It outputs human readable
+strings of the top 5 predictions along with their probabilities.
+
+Change the --image_file argument to any jpg image to compute a
+classification of that image.
+
+Please see the tutorial and website for a detailed description of how
+to use this script to perform image recognition.
+
+https://tensorflow.org/tutorials/image_recognition/
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os.path
+import re
+import sys
+import tarfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+import glob
+from params import *
+
+FLAGS = tf.app.flags.FLAGS
+
+# classify_image_graph_def.pb:
+#   Binary representation of the GraphDef protocol buffer.
+# imagenet_synset_to_human_label_map.txt:
+#   Map from synset ID to a human readable string.
+# imagenet_2012_challenge_label_map_proto.pbtxt:
+#   Text representation of a protocol buffer mapping a label to synset ID.
+tf.app.flags.DEFINE_string(
+    'model_dir', '/tmp/imagenet',
+    """Path to classify_image_graph_def.pb, """
+    """imagenet_synset_to_human_label_map.txt, and """
+    """imagenet_2012_challenge_label_map_proto.pbtxt.""")
+tf.app.flags.DEFINE_string('image_file', '',   ### here you can indicate the image file !!
+                           """Absolute path to image file.""")
+tf.app.flags.DEFINE_integer('num_top_predictions', 5,
+                            """Display this many predictions.""")
+
+# pylint: disable=line-too-long
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+# pylint: enable=line-too-long
+
+output = open(deep_learning_data, "w")
+
+def create_graph():
+  """Creates a graph from saved GraphDef file and returns a saver."""
+  # Creates graph from saved graph_def.pb.
+  with tf.gfile.FastGFile(os.path.join(
+      FLAGS.model_dir, 'classify_image_graph_def.pb'), 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_images(path_to_training_data):
+  """Runs inference on images under a the path to the training set.
+
+  Args:
+    path_to_training_data: path to training data.
+
+  Returns:
+    Nothing
+  """
+
+  # Creates graph from saved GraphDef.
+  create_graph()
+
+  with tf.Session() as sess:
+    # Some useful tensors:
+    # 'softmax:0': A tensor containing the normalized prediction across
+    #   1000 labels.
+    # 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+    #   float description of the image.
+    # 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+    #   encoding of the image.
+    # Runs the softmax tensor by feeding the image_data as input to the graph.
+    softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+
+    path_length = len(path_to_training_data)
+    # use glob to grab the image paths and loop over them
+    for categoryPath in glob.glob(path_to_training_data + "*"):
+      category = categoryPath[path_length:]
+      for imagePath in glob.glob(categoryPath + "/*.jpg"):
+        
+        # extract the image ID (i.e. the unique filename) from the image
+        # path and load the image itself
+        imageID = category + "/" + imagePath[imagePath.rfind("/") + 1:]
+
+        # describe the image
+        image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        # write the features to file
+        features = [str(f) for f in predictions]
+        output.write("%s,%s\n" % (imageID, ",".join(features)))
+
+
+    # print("Vector length: ", len(predictions))
+
+    # for dim in range(len(predictions)):
+    #   print('%.5f ' % predictions[dim])
+
+
+def maybe_download_and_extract():
+  """Download and extract model tar file."""
+  dest_directory = FLAGS.model_dir
+  if not os.path.exists(dest_directory):
+    os.makedirs(dest_directory)
+  filename = DATA_URL.split('/')[-1]
+  filepath = os.path.join(dest_directory, filename)
+  if not os.path.exists(filepath):
+    def _progress(count, block_size, total_size):
+      sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+          filename, float(count * block_size) / float(total_size) * 100.0))
+      sys.stdout.flush()
+    filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+    print()
+    statinfo = os.stat(filepath)
+    print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
+  tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def main(_):
+  maybe_download_and_extract()
+  
+  run_inference_on_images(path_to_training_data)
+
+if __name__ == '__main__':
+  tf.app.run()
